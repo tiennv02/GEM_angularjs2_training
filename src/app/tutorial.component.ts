3e0366b4bc74df1f12953062fe4765eb7f02770e
Dinhmanh96 <Dinhmanh96@gmail.com>
2017-08-31 18:59:20 +0700

    Demo binding clas

--- src/app/tutorial.component.ts
+++ src/app/tutorial.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
 	selector: 'my-tutorial',
-	templateUrl: './tutorial.component.html'
+	templateUrl: './tutorial.component.html',
+	styleUrls: ['./tutorial.component.css']
 })
 export class TutorialComponent {
-	public name = 'GEM';
-	public input = 'Angular 2xx';
+
+	public apply = true;
 }
