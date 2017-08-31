1cabec20805a3b11b27297509cd2966d447320af
Dinhmanh96 <Dinhmanh96@gmail.com>
2017-08-31 18:17:53 +0700

    Demo One way binding Angular 2

---
+++ src/app/tutorial.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'my-tutorial',
+	templateUrl: './tutorial.component.html'
+})
+export class TutorialComponent {
+	public name = 'GEM';
+}
